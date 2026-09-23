454b4574b5b00f6ecdf9fe120d9218d6fb627699
Ridwan Yusuf <ridwan.yusuf@whiteshield.com>
2026-09-23 14:18:47 +0100

    Reroute FE to render

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,26 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), tailwindcss()],
+
+    server: {
+      host: true,
+      port: 5173,
+
+      proxy: {
+        '/api': {
+          target: env.API_TARGET || 'http://localhost:8000',
+          changeOrigin: true,
+          secure: true,
+
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  }
 })
